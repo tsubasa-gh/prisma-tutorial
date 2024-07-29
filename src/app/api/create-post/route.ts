284d06f1a0a8edbb1439b4f9ff1d70ddb3b05f06
tsubasa-gh <your.email@example.com>
2024-07-30 08:23:14 +0900

    [add]投稿作成API作成

---
+++ src/app/api/create-post/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../../../lib/prisma";
+
+export async function POST(req: NextRequest) {
+    try {
+        const { content, authorId } = await req.json();
+
+        const post = await prisma.post.create({
+            data: { content, authorId }
+        });
+
+        return NextResponse.json(post);
+    } catch(error) {
+        console.error("投稿作成に失敗しました:", error);
+        return NextResponse.json({ error: '投稿作成失敗' }, { status: 400 });
+    }
+}
